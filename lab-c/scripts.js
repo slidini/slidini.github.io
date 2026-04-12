034c347ebea1fa1ee35c64488321c19e6f5ad5bd
slidini <lo57839@zut.edu.pl>
2026-04-12 17:53:17 +0200

    lab-c skonczone

--- lab-c/scripts.js
+++ lab-c/scripts.js
@@ -20,8 +20,7 @@ function getLocation(){
 
 document.getElementById("saveCurrentMap").addEventListener("click", function (){
   document.getElementById("mapSaved").innerHTML = "";
-  document.getElementById("map").innerHTML = "";
-  puzzleDropZone.innerHTML = "";
+  document.getElementById("puzzleDragDrop").innerHTML = "";
 
   leafletImage(map, function (err, rasterMapCanvas) {
     let puzzleWidth = rasterMapCanvas.width / 4;
@@ -41,8 +40,8 @@ document.getElementById("saveCurrentMap").addEventListener("click", function (){
         puzzlePiece.style.order = Math.floor(Math.random() * 100);
 
         puzzlePiece.addEventListener("dragstart", function (event){
-          event.dataTransfer.setData("target", puzzlePiece.id);
-        })
+          event.dataTransfer.setData("text", puzzlePiece.id);
+        });
 
         let puzzleContext = puzzlePiece.getContext("2d");
         puzzleContext.drawImage(rasterMapCanvas,
@@ -50,36 +49,42 @@ document.getElementById("saveCurrentMap").addEventListener("click", function (){
           0, 0, puzzleWidth, puzzleHeight);
         document.getElementById("mapSaved").appendChild(puzzlePiece);
 
-        document.getElementById("puzzleDragDrop").innerHTML += (`
-        <div id="place${i}-${j}" class="dropspots"></div>
-        `);
+        let dropDiv = document.createElement("div");
+        dropDiv.id = "place" + i + "-" + j;
+        dropDiv.className = "dropspots";
 
+        dropDiv.addEventListener("dragover", function(event){
+          event.preventDefault();
+        });
+
+        dropDiv.addEventListener("drop", function(event){
+          event.preventDefault();
+          let draggedPuzzleId = event.dataTransfer.getData("text");
+          let canvasWithPuzzle = document.getElementById(draggedPuzzleId);
+          console.log(event.target);
+          console.log("puzel o id", draggedPuzzleId);
+          console.log("znaleziony", canvasWithPuzzle);
+          if(event.target.classList.contains("dropspots") === true){
+            if(event.target.children.length === 0){
+              event.target.appendChild(canvasWithPuzzle);
+            }
+          }else{
+            if(event.target.children.length > 0){
+              alert("miejsce zajete");
+            }
+          }
+          checkForWin();
+        });
+
+        document.getElementById("puzzleDragDrop").appendChild(dropDiv);
       }
     }
   });
+  //document.getElementById("map").innerHTML = "";
 });
 
-let puzzleDropZone = document.getElementById("puzzleDragDrop");
 
-puzzleDropZone.addEventListener("dragover", function (event) {
-  event.preventDefault();
-});
-
-puzzleDropZone.addEventListener("drop", function (event) {
-  event.preventDefault();
-  let draggedPuzzleId = event.dataTransfer.getData("target");
-  let canvasWithPuzzle = document.getElementById(draggedPuzzleId);
-  console.log(event.target);
-  if(event.target.classList.contains("dropspots") === true){
-    if(event.target.children.length === 0){
-      event.target.appendChild(canvasWithPuzzle);
-    }
-  }else{
-    if(event.target.children.length > 0){
-      alert("miejsce zajete");
-    }
-  }
-
+function checkForWin() {
   let win = true;
   for(let i = 0; i < 4; i++){
     for(let j = 0; j < 4; j++){
@@ -87,19 +92,18 @@ puzzleDropZone.addEventListener("drop", function (event) {
       if(spaceNumber.children.length !== 0){
         if(spaceNumber.firstElementChild.id !== i + "-" + j){
           win = false;
-          console.log("win false wrong place")
+          console.log("wrong place")
         }
       }else if (spaceNumber.children.length === 0){
         win = false;
-        console.log("win false not every puzzle arranged")
       }
     }
   }
   if(win === true){
     notifyme("Congratulations, you won!");
-    console.log("win")
+    console.log("win");
   }
-})
+}
 
 
 
@@ -113,5 +117,11 @@ async function notifyme(notificationText) {
     await new Notification("Puzzle game", {body: notificationText});
   }
 }
+
+
+
+
+
+
 
 
